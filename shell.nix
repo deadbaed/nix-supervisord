091e8741385b88ad476b509c05a2b6037cec1484
Philippe Loctaux <p@philippeloctaux.com>
2025-11-20 00:17:04 +0100

    default.nix -> shell.nix

--- shell.nix
+++ shell.nix
@@ -1,1 +1,12 @@
-(import ./. { }).shell
+{
+sources ? import ./npins,
+pkgs ? import sources.nixpkgs { },
+}:
+
+pkgs.mkShellNoCC {
+  buildInputs = with pkgs; [
+    npins
+    lnav
+    nixfmt-tree
+  ];
+}
